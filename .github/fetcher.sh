1c7be75c8a7ff8f4786810416f00ad5a1faa83f5
a.kano <61396023+kano1101@users.noreply.github.com>
2022-07-12 22:57:10 +0900

    Delete fetcher.sh

--- .github/fetcher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -sLJO -H 'Accept: application/octet-stream' \
-    "https://$RECIPE_NOTE_TOKEN@api.github.com/repos/kano1101/recipe_note/releases/assets/$( \
-      curl -sL https://$RECIPE_NOTE_TOKEN@api.github.com/repos/kano1101/recipe_note/releases/tags/v0.0.3 \
-        | jq '.assets[] | select(.name | contains("darwin")) | .id')"
