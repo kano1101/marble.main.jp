e7b8f4b94d9e588929dd32d07fbfbbb9fc9fd26c
a.kano <61396023+kano1101@users.noreply.github.com>
2022-07-12 22:35:26 +0900

    Update fetcher.sh

--- .github/fetcher.sh
+++ .github/fetcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 curl -sLJO -H 'Accept: application/octet-stream' \
-    "https://$GITHUB_TOKEN@api.github.com/repos/kano1101/recipe_note/releases/assets/$( \
+    "https://$RECIPE_NOTE_TOKEN@api.github.com/repos/kano1101/recipe_note/releases/assets/$( \
       curl -sL https://$RECIPE_NOTE_TOKEN@api.github.com/repos/kano1101/recipe_note/releases/tags/v${1} \
         | jq '.assets[] | select(.name | contains("Mac")) | .id')"
